1bcb959c65e45c764b538a0aa4b90a3d64437dcf
Blair Preston <blair.preston@student.awesomeinc.org>
2022-03-02 19:07:40 +0000

    append text to weather container

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,41 @@
 const key = '94017407c1b8e8bc75ac701e2dbb7042';
 let userZip = '40509'; // need to change later
-const weatherLink = `api.openweathermap.org/data/2.5/weather?zip=${userZip},us&appid=${key}`;
+const weatherLink = `https://api.openweathermap.org/data/2.5/weather?zip=${userZip},us&appid=${key}`;
+let weatherData = [];
+let weatherState = {
+    city: [],
+    temperature: [],
+    condition: []
+}
+const cityP = document.createElement('p');
+const temperatureP = document.createElement('p');
+const conditionP = document.createElement('p');
+const weatherBlock = document.querySelector('#weatherContainer'); // id in html
 
 
 async function asyncAxios () {
     let request = await axios.get(weatherLink);
     console.log(request);
-    let weatherData = request.data;
-    
-    // setState(data)
-
+    weatherData = await request.data;
+    setState(weatherData);
 }
 
 asyncAxios(); // call function
+console.log(weatherData);
+
+function setState(weatherData) {
+    weatherState.city = weatherData.name;
+    const cityText = document.createTextNode(weatherState.city);
+    cityP.appendChild(cityText);
+    weatherBlock.appendChild(cityP);
+    
+
+    weatherState.temperature = weatherData.main.temp + "K";
+    weatherState.condition = weatherData.weather[0].description;
+    console.log(weatherState);
+}
+
+// const node = document.createElement("li");
+// const textnode = document.createTextNode("Water");
+// node.appendChild(textnode);
+// document.getElementById("myList").appendChild(node);
